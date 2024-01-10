d2bb09a78bcb17240a1e1a566687e11e3c0dc361
Tess <tess@DESKTOP-9F1581K>
2024-01-10 08:36:17 +0100

    remove card btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,9 +20,12 @@ class Tasks {
 }
 
 function displayTask(newTask) {
-    let ulTask = document.querySelector(".tasksDoing");
+    let ulDoing = document.querySelector(".tasksDoing");
+    let ulToDo = document.querySelector(".tasksToDo");
+    let ulDone = document.querySelector(".tasksDone");
 
     let task = document.createElement('li');
+    task.id = 'task_' + newTask.id
     task.classList.add('card');
 
     task.innerHTML = `
@@ -34,11 +37,31 @@ function displayTask(newTask) {
         <p class="card__name">${newTask.title}</p>
         <p class="card__desc">${newTask.description}</p>
         <p class="card__timeBox">${newTask.dueDate}</p> 
-        <p class="card__timeBox">${newTask.state}</p>
+        <p class="card__pending">${newTask.state}</p> 
     `;
-
-    ulTask.appendChild(task);
-    return task;
+    switch (newTask.state) {
+        case "To Do":
+            ulToDo.appendChild(task)
+            break;
+        case "Doing":
+            ulDoing.appendChild(task)
+            break; 
+        case "Done":
+            ulDone.appendChild(task)
+            break;   
+        default:
+            ulToDo.appendChild(task)
+            break;
+    }
+    // ulTask.appendChild(newTask.state);
+    // let btnEdit = task.querySelector('#btnEdit');
+    // let btnDelete = task.querySelector('#btnDelete');
+    // if (btnEdit && btnDelete) {
+    //     console.log("Boutons correctement définis dans la carte.");
+    // } else {
+    //     console.log("Erreur: Boutons non définis correctement dans la carte.");
+    // }   
+    // return task;
 }
 
 let editingTask = null;
@@ -49,22 +72,24 @@ function submitNewTask() {
         event.preventDefault();
         let taskTitle = document.getElementById("name").value;
         let taskDesc = document.getElementById("desc").value;
-        let taskState = document.getElementById("pending").value; 
-        let taskDueDate = document.getElementById("dueDate").value;
+        let taskState = document.getElementById("pending").value;
+        let taskDueDate = document.getElementById("dueDate").value; // Utilisez la valeur de la boîte de texte
+
         let taskIsUrgent = document.getElementById("isUrgent").checked;
         console.log("Je suis une variable: " + taskTitle + taskDesc + taskDueDate);
+
         if (editingTask) {
-            let tasksArray = JSON.parse(localStorage.getItem('tasks')) || [];
+            let tasksArray = JSON.parse(localStorage.getItem('Tasks')) || [];
             let editedTask = tasksArray.find(task => task.id === parseInt(editingTask.id.split('_')[1]));
 
             if (editedTask) {
                 editedTask.title = taskTitle;
                 editedTask.description = taskDesc;
                 editedTask.state = taskState;
-                editedTask.dueDate = taskDueDate;
+                editedTask.dueDate = taskDueDate; // Utilisez la valeur de la boîte de texte
                 editedTask.isUrgent = taskIsUrgent;
 
-                localStorage.setItem('tasks', JSON.stringify(tasksArray));
+                localStorage.setItem('Tasks', JSON.stringify(tasksArray));
             }
 
             editingTask = null;
@@ -73,27 +98,44 @@ function submitNewTask() {
             let newTask = new Tasks(taskTitle, taskDesc, taskState, taskDueDate, taskIsUrgent);
             saveTaskLocally(newTask);
             displayTask(newTask);
-            
         }
     })
 }
 submitNewTask()
 
 function editTask(task) {
-    let title = task.querySelector(/* to confirm */).textContent; // <=== class list-element-text correspond to ...
-    document.getElementById("name").value = title; // <=== ... id input form
-    let desc = task.querySelector(/* to confirm */).textContent;
+    let title = task.querySelector('.card__name').textContent;
+    document.getElementById("name").value = title;
+
+    let desc = task.querySelector('.card__desc').textContent;
     document.getElementById("desc").value = desc;
-    let state = task.querySelector(/* to confirm */).textContent;
+
+    console.log("task id = " + task.id);
+    let state = task.querySelector(`#pending_${task.id.split('_')[1]}`).textContent;
+
+    let selects = document.getElementById("pending");
+
+    for(let select of selects){
+
+        if(select.textContent === state){
+            console.log(select);
+            select.selected = true
+        }
+        
+    }
     document.getElementById("pending").value = state;
-    let dueDate = task.querySelector(/* to confirm */).textContent;
+
+    let dueDate = task.querySelector('.card__timeBox').textContent;
+    console.log(dueDate);
     document.getElementById("dueDate").value = dueDate;
-    let isUrgent = task.querySelector(/* to confirm */).textContent;
+
+    let isUrgentElement = task.querySelector('.fa-triangle-exclamation');
+    let isUrgent = isUrgentElement ? true : false;
     document.getElementById("isUrgent").checked = isUrgent;
 
-    let btn = document.getElementById('submitBtn');
+    let btn = document.getElementById('btnSubmit');
     btn.setAttribute("value", "Edit task");
-    
+
     editingTask = task;
 }
 
@@ -110,49 +152,62 @@ function loadTasksLocally() {
         displayTask(task);
     });
 }
-loadTasksLocally();
+
 
 function deleteTask(taskId) {
-    let tasksArray = JSON.parse(localStorage.getItem('tasks')) || [];
+    let tasksArray = JSON.parse(localStorage.getItem('Tasks')) || [];
     tasksArray = tasksArray.filter(task => task.id !== taskId);
-    localStorage.setItem('tasks', JSON.stringify(tasksArray));
+    localStorage.setItem('Tasks', JSON.stringify(tasksArray));
+
     let taskElement = document.getElementById('task_' + taskId);
     if (taskElement) {
         taskElement.remove();
     }
 }
 
-function DistanceDueDate() {
-    let dueDateInput = document.getElementById("dueDate");
-    let dueDateValue = dueDateInput.value;
-    let dueDate = new Date(dueDateValue);
-    let formattedDueDate = format(dueDate, "MM/dd/yyyy");
-    let timeDifference = formatDistance(new Date(), dueDate, { addSuffix: true });
-    console.log("Due Date:", formattedDueDate);
-    console.log("Time Difference:", timeDifference);
-    return(timeDifference)
-}
+// function DistanceDueDate() {
+//     let dueDateInput = document.getElementById("dueDate");
+//     let dueDateValue = dueDateInput.value;
+//     let dueDate = new Date(dueDateValue);
+//     let formattedDueDate = format(dueDate, "MM/dd/yyyy");
+//     let timeDifference = formatDistance(new Date(), dueDate, { addSuffix: true });
+//     console.log("Due Date:", formattedDueDate);
+//     console.log("Time Difference:", timeDifference);
+//     return (timeDifference)
+// }
 
-function setupStateBtn() {
-    /*
-    * get de otpion element on card task
-    * listen de change
-    * on change
-    * stwitch state, class, display, ...
-    */
-}
 
 function setupEditBtn() {
-    let editTaskButtons = document.querySelectorAll('.listContainer-listToDo-task-editBtn');
+    let editTaskButtons = document.querySelectorAll('#btnEdit');
     editTaskButtons.forEach(btn => {
         btn.addEventListener('click', () => {
-            let taskId = btn.parentNode.id.split('_')[1];
+            console.log("Setting up Edit button");
+            let taskId = btn.parentNode.parentNode.id.split('_')[1];
             let taskElement = document.getElementById('task_' + taskId);
             editTask(taskElement);
         });
     });
 }
 
-function setupDeleteBtn() {
 
+function setupDeleteBtn() {
+    console.log("Setting up delete fct");
+    let deleteTasksBtn = document.querySelectorAll('#btnDelete');
+    deleteTasksBtn.forEach(btn => {
+        btn.addEventListener('click', () => {
+            console.log("Setting up delete btn");
+            // Trouver l'élément de la tâche
+            let taskElement = btn.closest('.card');
+            if (taskElement) {
+                // Extraire l'ID de la tâche
+                let taskId = taskElement.id.split('_')[1];
+                console.log(taskId);
+                // Appeler la fonction de suppression
+                deleteTask(taskId);
+            }
+        });
+    });
 }
+loadTasksLocally();
+setupEditBtn();
+setupDeleteBtn();
